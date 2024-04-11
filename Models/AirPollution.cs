cbcff370f0fe8e7b0504a0ca8aa85d9f461bff7a
abdelaliadr <abdelaliadrif02@gmail.com>
2024-04-11 12:43:34 +0000

    Form created, Classes added

---
+++ Models/AirPollution.cs
@@ -0,0 +1,11 @@
+﻿namespace Eco_Tanger.Models
+{
+    public class AirPollution
+    {
+        public int airpollution_id { get; set; }
+        public int location_id { get; set; }
+        public string pollutant { get; set; } = "";
+        public string pollutant_level { get; set; } = "";
+        public DateTime timestamp { get; set; }
+    }
+}
---
+++ Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace Eco_Tanger.Models
+{
+    public class City
+    {
+        public int city_id { get; set; }
+        public string city_name { get; set; } = "";
+        public string country { get; set; } = "";
+    }
+}
