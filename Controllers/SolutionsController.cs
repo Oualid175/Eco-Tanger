5ce56d26964de6fcca891897e9a00b4479721b68
asus <asus@DESKTOP-QRDPND5>
2024-04-19 12:26:55 +0100

    Views-Solutions

---
+++ Controllers/SolutionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eco_Tanger.Controllers
+{
+    public class SolutionsController : Controller
+    {
+        public IActionResult AirPollutionSolutions()
+        {
+            return View();
+        }
+        public IActionResult ForestPollutionSolutions()
+        {
+            return View();
+        }
+        public IActionResult WasteWaterSolutions()
+        {
+            return View();
+        }
+    }
+}
